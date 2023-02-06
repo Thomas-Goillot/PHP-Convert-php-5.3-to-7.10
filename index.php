b765d52b7d3a8ba451ca7bc0865bc1693631da97
Thomas <222teegee@gmail.com>
2023-02-06 17:04:36 +0100

    Bug fix + add readline for user

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
 
-//get a Folder of a project
-$projectFolder = "C:\wamp\www\art315_prod";
+//$projectFolder = "C:\wamp\www\art315_prod";
 $tempFolder = "temp/";
 $return = "return/";
 
@@ -22,14 +21,33 @@ class Convert {
     */
     public $return;
 
-    public function __construct($projectFolder, $tempFolder){
+    public function __construct(string $projectFolder, string $tempFolder){
         $this->projectFolder = $projectFolder;
         $this->tempFolder = $tempFolder;
     }
 
-    public function copyToTempFolder($src, $dest)
-    {
+    public function dirIsEmpty(string $dir){
+        $handle = opendir($dir);
+        while (false !== ($entry = readdir($handle))) {
+            if ($entry != "." && $entry != "..") {
+                closedir($handle);
+                return false;
+            }
+        }
+        closedir($handle);
+        return true;
+    }
+
+    public function copyToFolder(string $src = "", string $dest = ""){
+
+        if($src === "") $src = $this->projectFolder;
+        if($dest === "") $dest = $this->tempFolder;
+
         $ignoreFileExtensions = array("pdf", "xls", "xlsx", "csv");
+
+        if(!$this->dirIsEmpty($dest)){
+            $this->clearFolder($this->tempFolder);
+        }
 
         foreach (scandir($src) as $file) {
 
@@ -38,7 +56,7 @@ class Convert {
                 if(!in_array($fileExtension, $ignoreFileExtensions)){
                     if (is_dir($src . '/' . $file)) {
                         mkdir($dest . '/' . $file, 0777, true);
-                        $this->copyToTempFolder($src . '/' . $file, $dest . '/' . $file);
+                        $this->copyToFolder($src . '/' . $file, $dest . '/' . $file);
                     } else {
                         copy($src . '/' . $file, $dest . '/' . $file);
                     }
@@ -47,13 +65,15 @@ class Convert {
         }
     }
 
-    public function clearTempFolder($src)
-    {
+    public function clearFolder(string $src = ""){
+
+        if($src === "") $src = $this->tempFolder;
+
         foreach (scandir($src) as $file) {
 
             if ($file != "." && $file != "..") {
                 if (is_dir($src . '/' . $file)) {
-                    $this->clearTempFolder($src . '/' . $file);
+                    $this->clearFolder($src . '/' . $file);
                     rmdir($src . '/' . $file);                    
                 } else {
                     unlink($src . '/' . $file);
@@ -61,11 +81,76 @@ class Convert {
             }
         }
     }
+
+    public function getAllFilesWithClass(){
+        $files = glob($this->tempFolder . '**/*.php');
+        $filesWithClass = array();
+        foreach ($files as $file) {
+            $content = file_get_contents($file);
+            $pattern = '/class\s+(\w+)/';
+            preg_match_all($pattern, $content, $matches);
+            if(count($matches[1]) > 0){
+                array_push($filesWithClass, $file);
+            }
+        }
+
+        return $filesWithClass;
+    }
+
+    public function replaceConstructor(string $file){
+        $content = file_get_contents($file);
+        $pattern = '/class\s+(\w+)/';
+        preg_match_all($pattern, $content, $matches);
+        $className = $matches[1][0];
+        $pattern = '/function\s+(\w+)/';
+        preg_match_all($pattern, $content, $matches);
+        $functionName = $matches[1][0];
+        if($className == $functionName){
+            $content = str_replace($functionName, "__construct", $content);
+            file_put_contents($file, $content);
+        }
+    }
+
+
 }
 
+//demander a l'utilisateur le chemin du dossier du projet
+$projectFolder = readline("Enter the path of the project folder:");
+
+echo "Project folder: " . $projectFolder . "\n";
+
+if(!is_dir($projectFolder)){
+    echo "This folder doesn't exist";
+    exit;
+}
+
+
 $convert = new Convert($projectFolder, $tempFolder);
-//$convert->copyToTempFolder($projectFolder, $tempFolder);
-$convert->clearTempFolder($tempFolder);
+
+//Clear all working folders
+$convert->clearFolder($return);
+$convert->clearFolder();
+
+//Copy files from project folder to temp folder
+$convert->copyToFolder();
+
+//Get all files with class
+$filesWithClass = $convert->getAllFilesWithClass();
+
+//Replace constructor name with __construct
+foreach ($filesWithClass as $file) {
+    $convert->replaceConstructor($file);
+}
+
+//Copy files from temp folder to return folder
+$convert->copyToFolder($tempFolder, $return);
+
+//Print all files edited
+print_r($filesWithClass);
+
+//Clear temp folder
+$convert->clearFolder();
+
 
 
 ?>
